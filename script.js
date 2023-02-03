119abb93bb6d50e512b9de2b50a6107e6c781fdb
lipeharakawa <felipeharakawa01@gmail.com>
2023-02-03 11:19:58 -0300

    Ajustes

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-var numeroSecreto = parseInt(Math.random() * 101);
+var jogar = prompt("Quer jogar?(s/n)");
+if(jogar == "s"){
+  
+  var numeroSecreto = parseInt(Math.random() * (101 - 1) + 1);
+  var iterador = 0;
 
-while (chute != numeroSecreto) {
-
-    var chute = prompt("Digite um número entre 0 e 100");
-
-    if (chute == numeroSecreto) {
-        alert("Acertou! ✅") 
-    } else if (chute > numeroSecreto) {
-        alert("Errou! ❌ O número secreto é menor")
-    } else if (chute < numeroSecreto) {
-        alert("Errou! ❌ O número secreto é maior")
-    }
-
-}
+  while(chute != numeroSecreto){
     
+    if(iterador != 0){
+      alert("Você já fez " + iterador + " tentativas!")
+    }
+    
+    var chute = prompt("Digite um número entre 1 e 100");
+    
+    if (chute == numeroSecreto){
+      alert("Acertou! ✅");
+    } else if (chute > numeroSecreto){
+         alert("Errou! ❌ O número secreto é menor que " + chute);
+    } else if (chute < numeroSecreto){
+         alert("Errou! ❌ O número secreto é maior que " + chute);
+    }
+    
+    iterador += 1;
+  }
+}
